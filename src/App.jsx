a09bb5c397f7ea86e35adec7da3d712cd8cd42b0
Adithya-aadhi <adithya.4818@gmail.com>
2025-10-08 14:34:51 +0530

    Consitinal rendering for game won is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,31 @@ import  clsx  from 'clsx'
 
 
 export default function AssemblyEndgame() {
+  //state
   const [currentWord,setCurrentWord]=useState("react")
-  const alphabets="abcdefghijklmnopqrstuvwxyz"
-
   const [guessed,setGuessed]=useState([])
-  console.log(guessed)
+
+  //derived 
+  const wrongGuess=guessed.filter(letter=>!currentWord.includes(letter)).length
+  const gameWon = currentWord.split("").every(letter =>guessed.includes(letter))
+  const gameLost = wrongGuess >= 8
+  const gameOver = gameWon || gameLost
+
+  //static
+  const alphabets="abcdefghijklmnopqrstuvwxyz"
 
   function addGuessed(letter){
       setGuessed(prev=>
-        prev.includes(letter)?prev:[...prev,letter]
+        prev.includes(letter)?prev:[...prev,letter] // check if it is already guessed or not 
       )
   }
 
+  //the keyboard 
   const keyEl=alphabets.split("").map((alph)=>{
     const isGuessed = guessed.includes(alph)
-    const iscorrect = isGuessed && currentWord.includes(alph)
+    const iscorrect = isGuessed && currentWord.includes(alph) //check with the current word i.e the word to guess 
     const iswrong = isGuessed && !currentWord.includes(alph)
+
     const className = clsx({
       correct : iscorrect,
       wrong : iswrong
@@ -35,17 +44,18 @@ export default function AssemblyEndgame() {
     )}
   )
 
-  const langEl=languages.map(lang =>{
+  const langEl=languages.map((lang,index) =>{
+    const isLost=index < wrongGuess
     const style={
       backgroundColor:lang.backgroundColor,
       color:lang.color
     }
     return(
-    <span style={style} className='chip' 
-    key={lang.name}> {lang.name}</span>)
+        <span style={style} className={`chip ${isLost?"lost":""}`} 
+        key={lang.name}> {lang.name}</span>)
   })
 
-  //the keyboard
+  //the letter to guess
   const letterEl=currentWord.split("").map((letter,index)=>(
     <span key={index}>{guessed.includes(letter)?letter.toUpperCase():""}</span>
   ))
@@ -57,9 +67,22 @@ export default function AssemblyEndgame() {
         <h1>Assembly Endgame</h1>
         <p>Guess the word to stfu</p>
       </header>
-      <section className="game-status">  
-         <h2>You win</h2>
-         <p>Congrats !!</p>
+      <section className={clsx("game-status",{
+        "win":gameWon,
+        "lose":gameLost
+      })}>  
+         {gameWon && (
+            <>
+              <h2>You Win!</h2>
+              <p>Congrats !!</p>
+            </>
+        )}
+          {gameLost && (
+            <>
+              <h2>You Lose!</h2>
+              <p>Better luck next time!</p>
+            </>
+          )}
       </section>
       <section className="lang-chips">
           {langEl}
@@ -70,7 +93,7 @@ export default function AssemblyEndgame() {
       <section className='keys'>
           {keyEl}
       </section>
-      <button className='newgame'>New game</button>
+      {gameOver && (<button className='newgame'>New game</button>)}
     </main>
   )
 }
